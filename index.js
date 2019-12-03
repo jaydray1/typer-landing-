39594da24545e1169a50c5c40b319ce48237909c
jaydray1 <jdreher15@gmail.com>
2019-12-02 19:43:24 -0800

    bring it back

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import styled from 'styled-components'
 import './style.css';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Detail = styled.p`  
+  font-size: 20px;
+  text-align: center; 
+  width: 50%;
+`;
+
+const Banner = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  @media only screen and (min-width: 768px) {
+    font-size: 5em;
+  }
+`;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      text: 'Jon-Michael J. Dreher'
+      text: 'Jon-Michael J. Dreher',
+      detail: 'I\'ve been working as a Software Developer with a focus on front end development for the last 2+ years.'
     };
   }
 
   render() {
     return (
-      <div>
-        <h1 style={{color: 'white'}}>{this.state.text}</h1>
-      </div>
+      <Container>
+        <Banner>{this.state.text}</Banner>
+        <Detail className="detail">{this.state.detail}</Detail>
+      </Container>
     );
   }
 }
